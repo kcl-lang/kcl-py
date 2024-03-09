176391dc77a432bae0aaed50be057941e80184da
peefy <xpf6677@163.com>
2024-03-09 11:27:56 +0800

    chore: bump version to 0.8.0-beta.1

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ with open(require_path) as f:
 setup(
     name="kclvm",
     author="KCL Authors",
-    version="0.8.0-alpha.6",
+    version="0.8.0-beta.1",
     license="Apache License 2.0",
     python_requires=">=3.7",
     description="KCL Python SDK",
